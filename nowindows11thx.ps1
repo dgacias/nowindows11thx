2983bcf495bb0ac19aff7496884e0308d8aeb3b4
David Gacias <dgacias@outlook.es>
2024-06-03 23:28:31 +0200

    Update nowindows11thx.ps1
    Removed additional logic to restore the execution policy

--- nowindows11thx.ps1
+++ nowindows11thx.ps1
@@ -1,9 +1,7 @@
-$PREVPOLICY=Get-ExecutionPolicy
-Set-ExecutionPolicy Unrestricted -Force
+Set-ExecutionPolicy -Scope Process Unrestricted -Force
 Install-Module -Name PolicyFileEditor
 Import-Module PolicyFileEditor
 Set-PolicyFileEntry -Path "C:\Windows\System32\GroupPolicy\Machine\Registry.pol" -Key "Software\Policies\Microsoft\Windows\WindowsUpdate" -ValueName "ProductVersion" -Data "Windows 10"
 Set-PolicyFileEntry -Path "C:\Windows\System32\GroupPolicy\Machine\Registry.pol" -Key "Software\Policies\Microsoft\Windows\WindowsUpdate" -ValueName "TargetReleaseVersion" -Data "1" -Type "DWord"
 Set-PolicyFileEntry -Path "C:\Windows\System32\GroupPolicy\Machine\Registry.pol" -Key "Software\Policies\Microsoft\Windows\WindowsUpdate" -ValueName "TargetReleaseVersionInfo" -Data "22H2"
 Remove-Module -Name PolicyFileEditor
-Set-ExecutionPolicy $PREVPOLICY -Force
